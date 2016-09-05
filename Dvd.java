a38a6e6b5753854f435b678b0c7deda89dda19a0
patinandroid <patina@dbmail.dk>
2016-09-05 10:00:25 +0200

    Dvd added

---
+++ Dvd.java
@@ -0,0 +1,17 @@
+package PACKAGE_NAME;
+
+import java.time.LocalTime;
+
+/**
+ * Created by tomas on 9/2/16.
+ */
+public class Dvd {
+	public String dvdTitle;
+
+	public Dvd() {
+		this.dvdTitle = "No title";
+	}
+	public Dvd(String dvdTitle){
+		this.dvdTitle=dvdTitle;
+	}
+}
